339375db75834a6be717f26b247b918e23405068
manuel högler <hoeglermanuel@gmail.com>
2020-10-06 06:42:27 +0200

    Second Commit printPrimme method

--- Primerechner/src/primerechner/EratosthenesPrimeSieve.java
+++ Primerechner/src/primerechner/EratosthenesPrimeSieve.java
@@ -16,11 +16,7 @@ public class EratosthenesPrimeSieve {
     int obergrenze;
     boolean prime[];
 
-    /**
-     *
-     * @param p
-     * @return 
-     */
+    
     public EratosthenesPrimeSieve(int obergrenze) {
         this.obergrenze = obergrenze;
     }
@@ -51,7 +47,7 @@ public class EratosthenesPrimeSieve {
      
             
         }
-     public void printPrime(int p){
+    public void printPrime(int p){
              prime = new boolean[p+1];
         for (int i = 0; i <p; i++) {
             prime[i]= true;
@@ -65,46 +61,7 @@ public class EratosthenesPrimeSieve {
                 }
             }
         }
-            for (int i = 2; i <=p; i++) {
-             if(prime[i]==true){
-                 System.out.println(i+"");
-             }
-             
-         }
-             
-          
-        }
-     
-       
-        public void primeCalc(int g){
-            int a = 0;
-            int b =0;
-            int[]alleZahlenArray = new int[g];
-            for (int i = 2; i <= (g/2); i++) { //
-                alleZahlenArray[i] = i*2;      //
-                
-            }
-            for (int i : alleZahlenArray) {
-                for (int j = 2; j < i; j++) {
-                    if(isPrime(j)==true){
-                        a=j;
-                        for (int k = 2; k < i; k++) {
-                            if(isPrime(k)==true &&(k+a)==i){
-                            b=k;
-                            if(a+b==i){
-                        System.out.println("erg:"+i+"="+a+"+"+b);
-                    }
-                        }
-                            
-                        }
-                    }
-                    
-                 
-                    
-            }
-        }
-         
+    }
 }
-        }
     
 
